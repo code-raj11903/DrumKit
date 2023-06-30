e1d17702b0f234265e30e2db4176441dee6113cf
Raj gupta <code.raj11903@gmail.com>
2023-06-30 20:02:28 +0530

    files added

--- index.js
+++ index.js
@@ -26,25 +26,25 @@ document.addEventListener("keydown", function(event){
 function maksound(key)
 {
     switch (key) {
-        case "w": var sound = new Audio("/sounds/tom-1.mp3");
+        case "w": var sound = new Audio("./sounds/tom-1.mp3");
                 sound.play();
             break;
-        case "a":var sound = new Audio("/sounds/tom-2.mp3");
+        case "a":var sound = new Audio("./sounds/tom-2.mp3");
         sound.play();
             break;
-        case "s":var sound = new Audio("/sounds/tom-3.mp3");
+        case "s":var sound = new Audio("./sounds/tom-3.mp3");
         sound.play();
             break;
-        case "d":var sound = new Audio("/sounds/tom-4.mp3");
+        case "d":var sound = new Audio("./sounds/tom-4.mp3");
         sound.play();
             break; 
-        case "j":var sound = new Audio("/sounds/snare.mp3");
+        case "j":var sound = new Audio("./sounds/snare.mp3");
         sound.play();
             break;
-        case "k":var sound = new Audio("/sounds/kick-bass.mp3");
+        case "k":var sound = new Audio("./sounds/kick-bass.mp3");
         sound.play();
             break;
-        case "l":var sound = new Audio("/sounds/crash.mp3");
+        case "l":var sound = new Audio("./sounds/crash.mp3");
         sound.play();
             break;
             
